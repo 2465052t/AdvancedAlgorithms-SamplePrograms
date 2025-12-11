d4d38ca4566314a4bd8df704a6876fdb8940b8cf
2465052t <167269982+2465052t@users.noreply.github.com>
2025-12-11 11:30:32 +0900

    Update balloon.c

--- kadai3/balloon.c
+++ kadai3/balloon.c
@@ -44,6 +44,7 @@ void setupBalloons(FILE * in, int n) {
   }
 }
 
+int distance[MAX_BALLOONS+1][4];
 /*
  * balloons配列に記録されている問題を解ける
  * solve the problem contained in the balloons array
@@ -52,10 +53,70 @@ void setupBalloons(FILE * in, int n) {
  * - n: 風船の数 number of balloons
  */
 result_t solve(int n) {
-  /* TODO */
+  /* Initialization */
+  for (int i = 0; i < n + 1; i++) {
+    for (int j = 0; j < 4; j++) {
+      distance[i][j] = INT_MAX;
+    }
+  }
+  distance[0][0] = 0;
+  int current_time = 0;
+  int current_pos = 0;
+  
+  for (int b = 0; b < n; b++) { /* Loop on the balloons */
+    balloon_t balloon = balloons[b];
+    int time_available = balloon.time - current_time;
 
-	result_t result = { true, 42 }; /* 全ての風船を回収出来た，かかった距離は42 */
-  // result_t result = {false, 3};/* 風船3個目は回収不能でした */
+    int flag_reached_next_balloon = 0;
+
+    for (int carrying = 0; carrying < 4; carrying++) { /* Loop on nb of balloons carried by robot */
+      if (distance[b][carrying] == INT_MAX) {
+        continue;
+      }
+
+      /* Check if going back home yields a shorter path */
+      int time_via_home = current_pos * (1 + carrying) + balloon.pos;
+      if (time_via_home <= time_available) {
+        int distance_via_home = distance[b][carrying] + current_pos + balloon.pos;
+        if (distance_via_home < distance[b+1][1]) {
+          distance[b + 1][1] = distance_via_home;
+          flag_reached_next_balloon = 1;
+        }
+      }
+      
+      if (carrying <= 2) {
+        /* Check if can go directly */
+        int dist_to_balloon = abs(balloon.pos - current_pos);
+        int time_direct = dist_to_balloon * (1 + carrying);
+        if (time_direct <= time_available) {
+          int distance_direct = distance[b][carrying] + dist_to_balloon;
+          distance[b + 1][carrying + 1] = distance_direct;
+          flag_reached_next_balloon = 1;
+        }
+      }
+    }
+    
+    /* Check if next ballon reached */
+    if (!flag_reached_next_balloon) {
+      return (result_t){ false, b + 1 };
+    }
+
+    /* Preparation for next iteration */
+    current_time = balloon.time;
+    current_pos = balloon.pos;
+  }
+
+  /* Find minimum distance to last balloon */
+  int min_distance = INT_MAX;
+  for (int carrying = 0; carrying < 4; carrying++) {
+    if (distance[n][carrying] < min_distance) {
+      min_distance = distance[n][carrying];
+    }
+  }
+  /* Add distance back to home */
+  min_distance += current_pos;
+	result_t result = { true, min_distance };
+
 	return result;
 }
 
